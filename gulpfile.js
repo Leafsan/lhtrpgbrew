03acf93cfd7fd97e5200e4d35b5e59ec18a65a61
Leafsan <cwk0906@naver.com>
2024-08-08 01:43:15 +0900

    package error fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-const gulp = require('gulp');
-const prefix = require('gulp-autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
-const sass = require('gulp-sass');
+const gulp = require("gulp");
+const prefix = require("gulp-autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+const sass = require("gulp-sass")(require("sass"));
 
 /* ----------------------------------------- */
 /*  Compile Sass
@@ -10,24 +10,24 @@ const sass = require('gulp-sass');
 // Small error handler helper function.
 function handleError(err) {
   console.log(err.toString());
-  this.emit('end');
+  this.emit("end");
 }
 
 const SYSTEM_SCSS = ["scss/**/*.scss"];
 function compileScss() {
   // Configure options for sass output. For example, 'expanded' or 'nested'
   let options = {
-    outputStyle: 'expanded'
+    outputStyle: "expanded",
   };
-  return gulp.src(SYSTEM_SCSS)
+  return gulp
+    .src(SYSTEM_SCSS)
+    .pipe(sass(options).on("error", handleError))
     .pipe(
-      sass(options)
-        .on('error', handleError)
+      prefix({
+        cascade: false,
+      })
     )
-    .pipe(prefix({
-      cascade: false
-    }))
-    .pipe(gulp.dest("./css"))
+    .pipe(gulp.dest("./css"));
 }
 const css = gulp.series(compileScss);
 
@@ -43,11 +43,6 @@ function watchUpdates() {
 /*  Export Tasks
 /* ----------------------------------------- */
 
-exports.default = gulp.series(
-  compileScss,
-  watchUpdates
-);
-exports.build = gulp.series(
-  compileScss
-);
+exports.default = gulp.series(compileScss, watchUpdates);
+exports.build = gulp.series(compileScss);
 exports.css = css;
